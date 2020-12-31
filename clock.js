2912b2cb68193b28ab14052aecd5ed0b4bb11f6d
kamooni <66233760+kamooni@users.noreply.github.com>
2020-12-31 13:14:55 +0900

    clock end

--- clock.js
+++ clock.js
@@ -1,6 +1,21 @@
 const clockConatinaer = document.querySelector(".js-clock");
 const clockTitle = clockConatinaer.querySelector(".js-title");
 
-function init() {}
+function getTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  //   innerText : 문자열 그대로를 리턴
+  //   innerHtml : 태그를 적용시켜서 리턴 즉 문자열을 html로 인식하여 리턴
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours} : ${
+    minutes < 10 ? `0${minutes}` : minutes
+  } : ${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
 
 init();
